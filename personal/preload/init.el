006aafcf1270e74eebac89ab84dd3a2f474f164c
pkpa <pkpa@topsoe.com>
2019-01-31 11:27:02 +0530

    removed extra repositories

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -2,10 +2,10 @@
 
 ;;
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
+;; (add-to-list 'package-archives
+;;	     '("melpa" . "http://melpa.org/packages/") t)
+;; (add-to-list 'package-archives
+;;	     '("org" . "http://orgmode.org/elpa/") t)
 
 ;; prefer utf-8
 (set-language-environment "UTF-8")
