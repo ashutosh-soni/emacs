0adbd45ce4e4d50f9eabcc57ce66f820c3f52116
Ashutosh Soni <viknat1994@gmail.com>
2019-12-31 13:09:15 +0530

    setup commit.

---
+++ transient/history.el
@@ -0,0 +1,13 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-log-refresh
+  ("-n256" "--decorate"))
+ (magit-pull nil)
+ (magit-push nil)
+ (magit-tag nil))
